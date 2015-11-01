02faa1dd82b003222803d6b22592bed2bb4c5c64
Nitesh Mishra <niteshmishra@niteshmishra.in>
2015-11-02 04:26:55 +0530

    feat(ngElementAccessible): added logic for functionality in ngElementAccessible and ngElementAccess directives

--- src/ng-element-accessible.js
+++ src/ng-element-accessible.js
@@ -9,34 +9,60 @@
     myModule.directive('ngElementAccessible', ['defaultConfig', function (defaultConfig) {
         return {
             restrict: 'EA',
-            scope: {},
             controller: ['$scope', function ($scope) {
-                $scope.childElements = this.childElements = [];
+                this.childElements = [];
                 this.registerElement = function (childElement) {
                     this.childElements.push(childElement);
+                    return (this.childElements.length - 1);
                 };
-                this.resetCounter = function (childElement) {
 
+                this.resetCounter = function (count) {
+                    this.counter = count;
+                };
+                this.moveNext = function (count) {
+                    this.counter = (this.counter + count) < this.maxLength ? this.counter + count : this.maxLength - 1;
+                    this.moveFocus(this.counter);
+                };
+                this.movePrev = function (count) {
+                    this.counter = (this.counter - count) > -1 ? this.counter - count : 0;
+                    this.moveFocus(this.counter);
+                };
+                this.moveFocus = function (index) {
+                    this.childElements[index][0].focus();
+                };
+
+                this.keyDownHandler = function (ev, count) {
+                    if (angular.isDefined(count) && !isNaN(count) && count !== this.counter) {
+                        this.resetCounter(count);
+                    }
+                    if (!ev.keyCode) {
+                        if (ev.which) {
+                            ev.keyCode = ev.which;
+                        } else if (ev.charCode) {
+                            ev.keyCode = ev.charCode;
+                        }
+                    }
+                    if (ev.keyCode) {
+                        if (this.prev.indexOf(ev.keyCode.toString()) > -1) {
+                            this.movePrev(1);
+                            ev.preventDefault();
+                            ev.stopPropagation();
+                        } else if (this.next.indexOf(ev.keyCode.toString()) > -1) {
+                            this.moveNext(1);
+                            ev.preventDefault();
+                            ev.stopPropagation();
+                        }
+                    }
                 };
             }],
             link: function (scope, elem, attr, ctrl) {
-                scope.prev = attr.prev ? attr.prev.split(',') : defaultConfig.prev.split(',');
-                scope.next = attr.next ? attr.next.split(',') : defaultConfig.next.split(',');
-                scope.maxLength = scope.childElements.length;
-                scope.counter = -1;
+                ctrl.prev = attr.prev ? attr.prev.split(',') : defaultConfig.prev.split(',');
+                ctrl.next = attr.next ? attr.next.split(',') : defaultConfig.next.split(',');
+                ctrl.maxLength = ctrl.childElements.length;
+                ctrl.counter = -1;
 
-                scope.moveNext = function (count) {
-
-                };
-                scope.movePrev = function (count) {
-
-                };
-                scope.moveFocus = function (index) {
-
-                };
-
-                elem.bind('keypress', function (ev) {
-
+                elem.bind('keydown', function (ev) {
+                    ctrl.keyDownHandler(ev, -1);
                 });
             }
         };
@@ -45,10 +71,14 @@
     myModule.directive('ngElementAccess', [function () {
         return {
             restrict: 'EA',
-            require: '^ngElementAccessible',
-            scope: {},
             link: function (scope, elem, attr, ctrl) {
-                ctrl.registerElement(elem);
+                var parentCtrl = (elem.parent() && elem.parent().controller('ngElementAccessible')) || undefined;
+                if (angular.isDefined(parentCtrl)) {
+                    var count = parentCtrl.registerElement(elem);
+                    elem.bind('keydown', function (ev) {
+                        parentCtrl.keyDownHandler(ev, count);
+                    });
+                }
             }
         };
     }]);
